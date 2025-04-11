64e60e308bd32d93ad833f3ee93517b84143fa62
Ahasan Ullah <ahasanullah451@gmail.com>
2025-04-11 18:39:31 +0600

    added key event listener the rock-paper-scissors game

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -10,6 +10,18 @@ document.querySelector('.js-scissor-button').addEventListener('click',()=>{
   playGame('scissors')
 });
 
+document.body.addEventListener('keydown',(event)=>{
+  if(event.key==='r'){
+    playGame('rock');
+  }
+  else if(event.key==='p'){
+    playGame('paper');
+  }
+  else if(event.key==='s'){
+    playGame('scissors');
+  }
+})
+
 
 
 let score=JSON.parse(localStorage.getItem('score')) || {
