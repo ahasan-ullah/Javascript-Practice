a15e89d2e6a06f330c2048615d59211d8dbfab98
Ahasan Ullah <ahasanullah451@gmail.com>
2025-04-12 13:15:41 +0600

    dynamic image added according to the weather to weather app

--- Mini-Projects/Weather-App/script.js
+++ Mini-Projects/Weather-App/script.js
@@ -3,7 +3,7 @@ const apiURL="https://api.openweathermap.org/data/2.5/weather?units=metric";
 
 const searchBox=document.querySelector('.search input');
 const searchBtn=document.querySelector('.search button');
-
+const weatherIcon=document.querySelector('.weather-icon');
 
 async function checkWeather(city) {
   // const response=await fetch(apiURL+`&appid=${apiKey}`);
@@ -21,6 +21,21 @@ function updateDetails(data){
   document.querySelector('.humidity').innerHTML=data.main.humidity+'%';
   document.querySelector('.wind').innerHTML=data.wind.speed+'km/h';
 
+  if(data.weather[0].main==='Clouds'){
+    weatherIcon.src='images/clouds.png';
+  }
+  else if(data.weather[0].main==='Clear'){
+    weatherIcon.src='images/clear.png';
+  }
+  else if(data.weather[0].main==='Rain'){
+    weatherIcon.src='images/rain.png';
+  }
+  else if(data.weather[0].main==='Drizzle'){
+    weatherIcon.src='images/drizzle.png';
+  }
+  else if(data.weather[0].main==='Mist'){
+    weatherIcon.src='images/mist.png';
+  }
 }
 
 searchBtn.addEventListener('click',()=>{
